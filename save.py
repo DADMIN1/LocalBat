b92b461d41615c043ae291b98ec563971e5ec8c6
DADMIN1 <139256274+DADMIN1@users.noreply.github.com>
2024-10-17 17:04:36 -0800

    refactored general-purpose functions to save.py

---
+++ save.py
@@ -0,0 +1,53 @@
+import pathlib
+import json
+
+
+cwd = pathlib.Path.cwd()
+savedir = cwd / "savedir"
+if not savedir.exists(): savedir.mkdir()
+
+sub_savedirs = [savedir / subdir for subdir in ("jsonDumps", "javaFiles")]
+for subdir in sub_savedirs: 
+    if not subdir.exists(): subdir.mkdir()
+
+# builtin 'str.capitalize' forces all other letters to lowercase
+def Capitalize(text: str): return text[0].upper() + text[1:]
+
+def SaveFile(data, section_name, problem_name):
+    section_dir = sub_savedirs[0] / section_name
+    if not section_dir.exists(): section_dir.mkdir()
+    filepath = section_dir / str(problem_name + ".json")
+    print(f"saving to {filepath.name}")
+    with open(filepath, "w") as file:
+        json.dump(data, file, indent=2)
+    return
+
+
+# re-load the raw json
+def LoadFile(section_name, problem_name) -> dict:
+    section_dir = sub_savedirs[0] / section_name
+    filepath = section_dir / str(problem_name + ".json")
+    print(f"loading {filepath}")
+    jsonData = None
+    with open(filepath, "r", encoding="utf-8") as file:
+        jsonData = json.load(file)
+    return jsonData
+
+
+def WriteSitemapFile(sitemap_data: dict):
+    sitemap_file = cwd / 'sitemap.json'
+    with open(sitemap_file, 'w', encoding="utf-8") as file:
+        json.dump(sitemap_data, file, indent=2)
+        print("wrote sitemap file")
+    return
+
+
+def LoadSitemapFile() -> dict | None:
+    print("loading sitemap")
+    sitemap_file = cwd / 'sitemap.json'
+    sitemap_dict = None
+    if sitemap_file.exists():
+        with open(sitemap_file, "r", encoding="utf-8") as file:
+            sitemap_dict = json.load(file)
+    return sitemap_dict
+
