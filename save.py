93beb4c6431dadfe7d45021da7f9e2b991d464ee
DADMIN1 <139256274+DADMIN1@users.noreply.github.com>
2024-10-27 14:20:53 -0800

    Created difficulty ranking moved sitemap into savedir reformatted readme

--- save.py
+++ save.py
@@ -36,7 +36,7 @@ def LoadFile(section_name, problem_name) -> dict | None:
 
 
 def WriteSitemapFile(sitemap_data: dict):
-    sitemap_file = cwd / 'sitemap.json'
+    sitemap_file = savedir / 'sitemap.json'
     with open(sitemap_file, 'w', encoding="utf-8") as file:
         json.dump(sitemap_data, file, indent=2)
         print("wrote sitemap file")
@@ -45,10 +45,42 @@ def WriteSitemapFile(sitemap_data: dict):
 
 def LoadSitemapFile() -> dict | None:
     print("loading sitemap")
-    sitemap_file = cwd / 'sitemap.json'
+    sitemap_file = savedir / 'sitemap.json'
     sitemap_dict = None
     if sitemap_file.exists():
         with open(sitemap_file, "r", encoding="utf-8") as file:
             sitemap_dict = json.load(file)
     return sitemap_dict
+
+
+def GetProblemLists() -> dict:
+    sitemap = LoadSitemapFile()
+    if not sitemap:
+      print("[WARNING] sitemap-file not found. Could not construct problem lists.")
+      return {}
+    return { 
+      section_name: [*section_data['pages'].keys()] 
+      for (section_name, section_data) in sitemap.items() 
+    }
+
+
+# returns dict of { section-name: dict[problem_name, difficulty] }; sorted by difficulty
+def ConstructDifficultyTable(saveResult:bool = False) -> dict[str, dict[str, float]]:
+    section_manifests = GetProblemLists()
+    difficulty_table = {
+      section_name: { 
+        k:v for (k,v) in sorted({
+          problem_name: float(LoadFile(section_name, problem_name)["difficulty"])
+          for problem_name in problem_list
+        }.items(), key=lambda item:item[1]) # sorting by difficulty-value
+      } for (section_name, problem_list) in section_manifests.items()
+    }
+    
+    if saveResult:
+      save_path = savedir / "difficulty_table.json"
+      with open(save_path, 'w', encoding="utf-8") as save_file:
+        json.dump(difficulty_table, save_file, indent=2)
+        print(f"wrote '{save_path.relative_to(cwd)}'")
+    
+    return difficulty_table
 
